eae8ecb6a87c5218338c0ce2a6301e4a500786d0
Archit Gupta <60537742+Architgupta20@users.noreply.github.com>
2025-05-29 03:19:13 +0530

    Update main.py

--- orchestrator/main.py
+++ orchestrator/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 from orchestrator.orchestrator import process_query
+import shutil
 
 app = FastAPI()
 
 @app.post("/ask")
-async def ask(audio: UploadFile):
-    return {"response": process_query(audio)}
+async def ask(audio: UploadFile = File(...)):
+    with open("temp.wav", "wb") as buffer:
+        shutil.copyfileobj(audio.file, buffer)
+    response = process_query("temp.wav")
+    return {"response": response}
