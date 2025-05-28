2b3536ade70fd57d6febc823a884a62eec795f1b
Archit Gupta <60537742+Architgupta20@users.noreply.github.com>
2025-05-29 01:56:06 +0530

    Update main.py

--- orchestrator/main.py
+++ orchestrator/main.py
@@ -1,14 +1,8 @@
-# FastAPI backend
-
-#from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile
 from orchestrator.orchestrator import process_query
 
 app = FastAPI()
 
 @app.post("/ask")
 async def ask(audio: UploadFile):
-    file_path = "input.wav"
-    with open(file_path, "wb") as f:
-        f.write(await audio.read())
-    result = process_query(file_path)
-    return {"response": result}
+    return {"response": process_query(audio)}
